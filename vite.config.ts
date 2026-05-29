a3d88ace0cf646a38d9421c8f3421893e6ba980d
PixelMini play <itzdhirajchoudhary07@gmail.com>
2026-05-29 09:30:53 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: 'Shift-loop',
+    base: '/Shift-loop/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
